c9a67bc44221231169b8f645cdd21a401294d4c0
Arek Cios <arek123113@gmail.com>
2020-11-12 21:07:46 +0100

    Add getLaunchesBySearch.js

---
+++ pages/api/lib/getLaunchesBySearch.js
@@ -0,0 +1,15 @@
+async function getLaunchesBySearch(limit, offset, search) {
+    BASE_URL = 'https://ll.thespacedevs.com/2.0.0';
+
+    const result = await fetch(BASE_URL
+        + '/launch?limit=' + limit
+        + '&offset=' + offset
+        + '&search=' + search, 
+        { method: 'GET' });
+
+    const data = await result.json();
+    
+    return data;
+}
+
+export default getLaunchesBySearch;
