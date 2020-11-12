a05ea23f9eb551f9ccb2e0ea489b0ed11583dafd
Arek Cios <arek123113@gmail.com>
2020-11-12 21:04:40 +0100

    Add getAllLaunches.js

---
+++ pages/api/lib/getAllLaunches.js
@@ -0,0 +1,11 @@
+async function getAllLaunches(limit, offset) {
+    BASE_URL = 'https://ll.thespacedevs.com/2.0.0';
+
+    const result = await fetch(BASE_URL + '/launch?limit=' + limit + '&offset=' + offset, 
+        { method: 'GET' });
+    const data = await result.json();
+    
+    return data;
+}
+
+export default getAllLaunches;
