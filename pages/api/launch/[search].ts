babf0085ea873505d6dd47095da2d4a25c414009
Arek Cios <arek123113@gmail.com>
2020-11-16 19:32:22 +0100

    Implement launch searching in api

--- pages/api/launch/[search].ts
+++ pages/api/launch/[search].ts
@@ -1,9 +1,16 @@
+import getLaunchesBySearch from '../lib/getLaunchesBySearch'
+
 export default async (req, res) => {
     const {
       query: { search },
     } = req
 
+    const launches = await getLaunchesBySearch('100', '0', 'Vega');
+
     res.statusCode = 200
-    res.json({ name: search })
+    res.json({ 
+      status: "success",
+      data: launches
+     })
   }
   
--- pages/api/lib/getLaunchesBySearch.js
+++ pages/api/lib/getLaunchesBySearch.js
@@ -1,5 +1,5 @@
 async function getLaunchesBySearch(limit, offset, search) {
-    BASE_URL = 'https://ll.thespacedevs.com/2.0.0';
+    const BASE_URL = 'https://ll.thespacedevs.com/2.0.0';
 
     const result = await fetch(BASE_URL
         + '/launch?limit=' + limit
