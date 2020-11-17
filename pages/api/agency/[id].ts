76206ef8c624fa127e6efa9bad7ca35da8254a52
Arek Cios <arek123113@gmail.com>
2020-11-17 19:46:40 +0100

    Update api/agency/[id].ts and api/agency/index.ts

--- pages/api/agency/[id].ts
+++ pages/api/agency/[id].ts
@@ -1,9 +1,14 @@
-export default (req, res) => {
+import getAgencyById from '../lib/getAgnecyById';
+
+export default async (req, res) => {
     const {
       query: { id },
     } = req
 
+    const agency = await getAgencyById(id);
     res.statusCode = 200
-    res.json({ name: id })
+    res.json({ 
+      status: 'success',
+      data: agency })
   }
   
--- pages/api/agency/index.ts
+++ pages/api/agency/index.ts
@@ -1,5 +1,13 @@
-export default (req, res) => {
+import getAllAgencies from '../lib/getAllAgencies';
+
+export default async (req, res) => {
+    const agencies = await getAllAgencies('99999', '0');
+    //TILL PAGINATION IS NOT IMPLEMENTED
+    //GONNA KEEP IT LIKE THAT
+
     res.statusCode = 200
-    res.json({ name: 'John Doe hello' })
+    res.json({ 
+      status: 'success',
+      data: agencies })
   }
   
