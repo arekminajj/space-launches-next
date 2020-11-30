5585172179e406009aca7c55dfc45700d952a774
Arek Cios <arek123113@gmail.com>
2020-11-30 11:19:13 +0100

    Add launches/upcoming.ts page

--- pages/launch/upcoming.tsx
+++ pages/launch/upcoming.tsx
@@ -1,4 +1,12 @@
 import Head from 'next/head'
+import Link from 'next/link'
+
+//Bootstrap imports
+import Container from 'react-bootstrap/Container';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 export async function getStaticProps(context) {
     const res = await fetch(process.env.BASE_URL + '/api/launch/upcoming');
@@ -11,6 +19,29 @@ export async function getStaticProps(context) {
 
 export default function Upcoming({launches}) {
   return (
-    <div>{launches.data.results[0].name}</div>
+    <Container>
+      <div style={{height: 15}}></div>
+      <h1 style={{textAlign: "center"}}>Upcoming Launches</h1>
+      <Row>
+      {launches.data.results.map(function (d, idx) {
+                        return ( 
+                          <Col>
+                          <Card style={{ width: '18rem' }}>
+                          <Card.Img variant="top" src={d.image} />
+                            <Card.Body>
+                              <Card.Title>{d.name}</Card.Title>
+                              <Card.Text>
+                              {d.window_start}
+                              </Card.Text>
+                              <Link href={"id/" + d.id}>
+                                <Button variant="primary">Details</Button>
+                              </Link>
+                            </Card.Body>
+                          </Card>
+                          <div style={{height: 15}}></div>
+                          </Col>                  
+      )})}
+      </Row>
+    </Container>
   )
 }
